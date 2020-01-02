a9c4a98e7c4eaa421b96aa34eba87b77a3dd26bf
Catherine Pedersen <cathy.pedersen@gmail.com>
2020-01-01 23:16:01 -0500

    Fixed error returned when using 1 Max Result

--- index.js
+++ index.js
@@ -19,19 +19,24 @@ function formatQueryParams(params) {
 function displayResults(queryPark, queryStates, queryMax, responseJson) {
   const values = Object.values(responseJson);
   $('#results-list').empty();
-  $('#results-list').append(`<p>${responseJson.total} parks found using query '${queryPark}'</p>`);
 
-  for (let i = 0; i < responseJson.total ; i++){
+  if (queryMax < responseJson.total) {
+    const num_items = queryMax;
+    $('#results-list').append(`<p>${queryMax} parks found using query '${queryPark}'</p>`);
+  } else {
+    $('#results-list').append(`<p>${responseJson.total} parks found using query '${queryPark}'</p>`);
+  }
+
+  for (let i = 0; i < responseJson.total && i < queryMax; i++){
     $('#results-list').append(
-      `<li>
-      <h2>Park: ${responseJson.data[i].fullName}</h2>
-      <p>Description: ${responseJson.data[i].description}</p>
-      <p>States: ${responseJson.data[i].states}</p>
-      <p>URL: <a href="${responseJson.data[i].url}">${responseJson.data[i].url}</a></p>
-      </li>`
+        `<li>
+        <h2>Park: ${responseJson.data[i].fullName}</h2>
+        <p>Description: ${responseJson.data[i].description}</p>
+        <p>States: ${responseJson.data[i].states}</p>
+        <p>URL: <a href="${responseJson.data[i].url}">${responseJson.data[i].url}</a></p>
+        </li>`
     )};
-  
-
+    
   //display the results section  
   $('#results').removeClass('hidden');
 };
